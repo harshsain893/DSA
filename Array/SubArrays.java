a27a11e56cb102383f95039f987253154968940d
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-09-06 16:37:30 +0530

    updated subarrays

--- Array/SubArrays.java
+++ Array/SubArrays.java
@@ -3,9 +3,8 @@ public class SubArrays {
     public static void SubArrays(int number[]){
         for(int i = 0; i < number.length; i++){
             int start = i;
-            int curr = number[i];
             for(int j = i; j < number.length; j++){
-                int end = number[j];
+                int end = j;
                 for (int k = start; k <= end; k++){
                     System.out.print(number[k]+ " ");
                 }
