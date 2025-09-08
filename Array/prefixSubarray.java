b62dde6003996d3d9d8fcc8300e9758e3972b846
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-09-08 22:56:58 +0530

    In this code the time complexity of this code is O(n*n)

---
+++ Array/prefixSubarray.java
@@ -0,0 +1,16 @@
+public class prefixSubarray {
+    public static void prefixSubarray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            int sum = 0;
+            for(int j = i; j < arr.length; j++){  
+                sum = sum + arr[j];
+                System.out.println("Subarray (" + i + "," + j + ") sum = " + sum);
+            }       
+        }
+    }
+    // In this code the time complexity if the code is = O(n*n)
+    public static void main(String []args){
+        int arr[] = {2,4,6,8,10};
+        prefixSubarray(arr);
+    }
+}
