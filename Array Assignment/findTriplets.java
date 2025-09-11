1ebd649fec44697b85f7d4c30c49b9fc8b43bd06
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-09-11 11:40:32 +0530

    This logic brute force not a optimized logic

---
+++ Array Assignment/findTriplets.java
@@ -0,0 +1,20 @@
+public class findTriplets {
+    public static void findTriplets(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            for (int j = i; j < arr.length; j++){
+                for (int k = j+1; k < arr.length; k++){
+                    if(i !=  j && i != k && j != k){
+                        int add = arr[i] + arr[j] + arr[k];
+                        if(add == 0){
+                            System.out.println(arr[i]+" "+ arr[j] +" " + arr[k]);
+                        }
+                    } 
+                }
+            }
+        }
+    }
+    public static void main(String[]args){
+        int arr[] = {-1, 0, 1, 2, -1, -4};
+        findTriplets(arr);
+    }
+}
