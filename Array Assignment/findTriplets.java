45002ea301e537fde0a459a4c1214fa27660498d
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-09-12 10:38:28 +0530

    Updated code

--- Array Assignment/findTriplets.java
+++ Array Assignment/findTriplets.java
@@ -1,11 +1,11 @@
 public class findTriplets {
     public static void findTriplets(int arr[]){
         for(int i = 0; i < arr.length; i++){
-            for (int j = i; j < arr.length; j++){
+            for (int j = i+1; j < arr.length; j++){
                 for (int k = j+1; k < arr.length; k++){
                     if(i !=  j && i != k && j != k){
-                        int add = arr[i] + arr[j] + arr[k];
-                        if(add == 0){
+                        // int add = arr[i] + arr[j] + arr[k];
+                        if(arr[i] + arr[j] + arr[k] == 0){
                             System.out.println(arr[i]+" "+ arr[j] +" " + arr[k]);
                         }
                     } 
