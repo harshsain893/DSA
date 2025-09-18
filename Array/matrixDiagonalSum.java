3f0a188fe880d3cb3dca63e520d5b3139a81a91c
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-09-18 23:32:41 +0530

    Diagonal sum problem aand this is optimized code

---
+++ Array/matrixDiagonalSum.java
@@ -0,0 +1,22 @@
+package Array;
+public class matrixDiagonalSum {
+    public static int Diagonalsum(int matrix[][]){
+        int sum = 0;
+        for(int i = 0; i < matrix.length; i++){
+            sum += matrix[i][i];
+            if(i != matrix.length-1-i){
+                sum += matrix[i][matrix.length-i-1];
+            } 
+        }
+        return sum;
+    }
+    public static void main(String []args){
+        int matrix[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16},
+        };
+        System.out.println(Diagonalsum(matrix));
+    }
+}
