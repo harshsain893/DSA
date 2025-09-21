5948ef44b0c0bf549b7c445ea85a2e987c03584d
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-09-21 16:01:37 +0530

    Learning String compression

---
+++ Strings/stringCompression.java
@@ -0,0 +1,26 @@
+package Strings;
+import java.util.*;
+public class stringCompression {
+    public static String stringCom(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++) {
+            int count = 1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1){
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+     }
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the String: ");
+        String str = sc.nextLine();
+        
+        System.out.println(stringCom(str));
+    }
+}
