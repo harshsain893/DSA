6639ec0c57e512eb4e59170600dfaebca0d07800
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-10-03 17:27:25 +0530

    Add comments in kaden algo

--- Array/kadaneAlgo.java
+++ Array/kadaneAlgo.java
@@ -1,20 +1,35 @@
 public class kadaneAlgo {
+    // Function to implement Kadane's Algorithm
+    // It finds the maximum subarray sum in the given array
     public static void kadaneAlgo(int number[]){
-        int sum = 0, max = Integer.MIN_VALUE;
+        int sum = 0;  // This will store the running sum of the current subarray
+        int max = Integer.MIN_VALUE; // Initialize max as the smallest possible integer value
+        
+        // Traverse the entire array
         for (int i = 0; i < number.length; i++){
-            sum += number[i];
+            sum += number[i];  // Add current element to running sum
+            
+            // If the running sum is greater than the max found so far, update max
             if (sum > max){
                 max = sum;
             }
+            
+            // If the running sum becomes negative, reset it to 0
+            // because continuing with a negative sum will only decrease future sums
             if (sum < 0){
                 sum = 0;
             }
-            
         }
+        
+        // After the loop, max contains the maximum subarray sum
         System.out.println(max);
     } 
-     public static void main (String []args){
-        int number[] = {-2,-3,-4,-5, -4};
+    
+    public static void main (String []args){
+        // Example array containing all negative numbers
+        int number[] = {-2, -3, -4, -5, -4};
+        
+        // Call the function to compute maximum subarray sum
         kadaneAlgo(number);
     }   
 }
