8c6abb20f9bec78f6555bc0fad7235b250b6576d
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-10-15 15:30:19 +0530

    Leetcode Question - 73

---
+++ Array/setMatrixZeroes.java
@@ -0,0 +1,27 @@
+public class setMatrixZeroes {
+    
+    public void setZeroes(int[][] matrix) {
+
+        // Brute Force Approach
+        int row[] = new int [matrix.length];
+        int col[] = new int [matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    row [i]= 1;
+                    col [j]= 1;
+                }
+            }
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // Optimal Approach
+    }
+}
+
