5e5589380dc73243179d9f9ce21634ca97aac92c
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-10-20 16:32:52 +0530

    Add comments in this code

--- Array/majorityElem.java
+++ Array/majorityElem.java
@@ -2,15 +2,27 @@ package Array;
 
 public class majorityElem {
     
+    // Function to find the majority element in the given array
+    // Majority element: the element that appears more than n/2 times
     public int majorityElement(int[] nums) {
-        int freq = 0 , ans = 0;
+        int freq = 0, ans = 0; // 'freq' keeps track of count balance, 'ans' stores the current candidate
 
-        for (int i = 0; i < nums.length; i++){
-            if (freq == 0) ans = nums[i];
-            if (ans == nums[i]) freq++;
-            else freq--;
+        // Loop through all elements of the array
+        for (int i = 0; i < nums.length; i++) {
+            // If frequency becomes zero, assume current number as potential majority candidate
+            if (freq == 0) 
+                ans = nums[i];
+            
+            // If the current number matches the candidate, increment frequency
+            if (ans == nums[i]) 
+                freq++;
+            // Otherwise, decrement frequency
+            else 
+                freq--;
         }
+
+        // After the loop, 'ans' will hold the majority element
+        // (Boyer–Moore Voting Algorithm)
         return ans;
     }
 }
-
