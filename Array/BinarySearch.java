3c163953384464b779a1a4dcd9e608466c66eebc
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-10-26 17:49:19 +0530

    Add comments in this code

--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -1,26 +1,41 @@
 package Array;
-public class BinarySearch {
-    public static int BinarySearch(int number[],int key){
-        int start = 0, end = number.length-1;
-        while(start <= end){
-            int mid = (start + end)/2;
 
-            // Comparisons
-            if (number[mid] == key){ //found
+public class BinarySearch {
+    
+    // Function to perform Binary Search on a sorted array
+    public static int BinarySearch(int number[], int key) {
+        int start = 0;                   // Starting index of the search range
+        int end = number.length - 1;     // Ending index of the search range
+
+        // Loop continues until the search range is valid
+        while (start <= end) {
+            int mid = (start + end) / 2; // Calculate middle index
+
+            // Check if the middle element is the key
+            if (number[mid] == key) {    // Element found
                 return mid;
             }
-            if(number[mid] < key){ //right
+
+            // If key is greater, ignore the left half
+            if (number[mid] < key) {
                 start = mid + 1;
-            }
-            else{
-                end = mid - 1; //left
+            } 
+            // If key is smaller, ignore the right half
+            else {
+                end = mid - 1;
             }
         }
+
+        // If key not found, return -1
         return -1;
     }
-    public static void main(String[]args){
-        int number[] = {2, 4, 6, 8, 10};
-        int key = 10;
-        System.out.println("Key is at Index : "+BinarySearch(number,key));
+
+    // Main function to test Binary Search
+    public static void main(String[] args) {
+        int number[] = {2, 4, 6, 8, 10}; // Sorted array (required for binary search)
+        int key = 10;                    // Element to search for
+
+        // Print the result
+        System.out.println("Key is at Index: " + BinarySearch(number, key));
     }
 }
