ecf09b19c671bfcf2dfda91b5bd9e3db7fb3008b
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-10-30 22:44:17 +0530

    Adding comments in this code

--- Strings/concatenation.java
+++ Strings/concatenation.java
@@ -1,18 +1,33 @@
-package Strings;
-public class concatenation {
+package Strings; // Package declaration (the class belongs to the 'Strings' package)
+
+public class concatenation { // Class definition
+
+    // Method to print each character of a string separated by space
     public static void print(String str){
+        // Loop through each character in the string
         for(int i = 0; i < str.length(); i++){
-            System.out.print(str.charAt(i)+ " ");
+            // Print each character followed by a space
+            System.out.print(str.charAt(i) + " ");
         }
+        // Print a new line after the loop ends
         System.out.println();
     }
+
     public static void main(String args[]){
+        // Declare and initialize first name
         String firstname = "Harsh";        
-        String lastname = "Sain";
-        // Concatenation
-        String fullname = firstname + " " + lastname;
-        // System.out.println(fullname.charAt(3)); 
         
+        // Declare and initialize last name
+        String lastname = "Sain";
+        
+        // Concatenate first name and last name with a space in between
+        String fullname = firstname + " " + lastname;
+        
+        // Example of accessing a specific character in the string
+        // System.out.println(fullname.charAt(3)); // Uncomment to print character at index 3
+        
+        // Call the print method to display each character of fullname
         print(fullname);
     }
 }
+
