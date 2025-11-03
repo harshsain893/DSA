6e4941972e2fdc05a7efa2eb3ea04e0d21acc1f7
Harsh Vardhan Sain <harshsain893@gmail.com>
2025-11-03 23:52:46 +0530

    Add comments in this code

--- Strings/pallindrome.java
+++ Strings/pallindrome.java
@@ -1,16 +1,27 @@
 package Strings;
+
 public class pallindrome {
-    public static boolean pallindrome(String str){
-        for(int i = 0; i < str.length()/2; i++){
-            int n = str.length();
-            if(str.charAt(i) != str.charAt(n-1-i)){
+
+    // Method to check if a given string is a palindrome
+    public static boolean pallindrome(String str) {
+        // Loop runs only till the middle of the string
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length(); // Get the total length of the string
+
+            // Compare the i-th character from start and i-th from end
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                // If any characters don't match, it's not a palindrome
                 return false;
             }
         }
+        // If all characters match, return true (it is a palindrome)
         return true;
     }
-   public static void main(String args[]){
-    String str = "racecar";
-    System.out.println(pallindrome(str));
-   } 
+
+    // Main method - program execution starts here
+    public static void main(String args[]) {
+        String str = "racecar"; // Input string to check
+        // Print whether the string is palindrome or not
+        System.out.println(pallindrome(str));
+    }
 }
