c84e6ddfac529289835113d80cba6235ac3f6cd8
Harsh Vardhan Sain <harshsain893@gmail.com>
2026-01-28 20:51:43 +0530

    this is a leetcode problem number - 162

---
+++ Array/findPeakElem.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;      // peak is on left
+            } else {
+                low = mid + 1;   // peak is on right
+            }
+        }
+        return low; // or high (both same)
+    }
+}
